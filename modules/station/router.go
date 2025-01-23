17f0e0b27e572fef99ae835173c8e65c67831615
= <=>
2025-01-23 21:59:42 +0700

    refactor: standardize station service initiation and API response handling

--- modules/station/router.go
+++ modules/station/router.go
@@ -1,10 +1,23 @@
 package station
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func Initiate(router *gin.RouterGroup) {
+	stationService := NewService()
+
 	station := router.Group("/station")
 	station.GET("", func(c *gin.Context) {
-		// code service
+		GetAllStation(c, stationService)
 	})
+}
+
+func GetAllStation(c *gin.Context, service Service) {
+	datas, err := service.GetAllStation()
+	if err != nil {
+		// handle error
+	}
+
+	// balikin response
 }
