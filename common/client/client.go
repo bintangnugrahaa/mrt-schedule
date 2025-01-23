ca2ea7a0c59f434ee8e9db95370e08cf1d3db6c3
= <=>
2025-01-23 22:33:36 +0700

    refactor: rename and optimize HTTP request function for improved clarity and performance

--- common/client/client.go
+++ common/client/client.go
@@ -2,24 +2,22 @@ package client
 
 import (
 	"errors"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
-func onRequest(client *http.Client, url string) ([]byte, error) {
+func DoRequest(client *http.Client, url string) ([]byte, error) {
 	resp, err := client.Get(url)
 	if err != nil {
 		return nil, err
 	}
-
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		return nil, errors.New("unexpected status code: " + resp.Status)
 	}
 
-	// read body response
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
